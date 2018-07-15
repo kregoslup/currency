b34b984623181513657e8bff61e4217a6d6cc663
krego <mateusz.olczynski@gmail.com>
2018-07-15 17:32:45 +0200

    Add logging to parser class

--- src/main/java/com/currency/scraper/scraping/Parser.java
+++ src/main/java/com/currency/scraper/scraping/Parser.java
@@ -1,6 +1,8 @@
 package com.currency.scraper.scraping;
 
 import com.currency.scraper.entity.ExchangeRate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.net.URI;
@@ -8,6 +10,9 @@ import java.util.List;
 
 @Component
 public class Parser {
+
+    private static final Logger log = LoggerFactory.getLogger(Parser.class);
+
     public List<URI> parseExchangeLinks(String payload) {
 
     }
