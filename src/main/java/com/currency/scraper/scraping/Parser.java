b514cb049f5e991ca29d28676d3a68819cc95679
Mateusz Olczynski <mateusz.olczynski@toreforge.com>
2018-07-16 14:48:29 +0200

    Add links and exchange rate parser

--- src/main/java/com/currency/scraper/scraping/Parser.java
+++ src/main/java/com/currency/scraper/scraping/Parser.java
@@ -1,21 +1,61 @@
 package com.currency.scraper.scraping;
 
 import com.currency.scraper.entity.ExchangeRate;
+import com.currency.scraper.vo.ExchangeLink;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class Parser {
 
     private static final Logger log = LoggerFactory.getLogger(Parser.class);
 
-    public List<URI> parseExchangeLinks(String payload) {
+    private static final URI LINK_TEMPLATE = URI.create("/rss/fxref");
 
+    public List<ExchangeLink> parseExchangeLinks(String payload) {
+        Document doc = Jsoup.parse(payload);
+        Elements links = doc.select("a");
+        return links.stream()
+                .filter(link -> link.attr("abs:href").startsWith(LINK_TEMPLATE.toString()))
+                .map(exchangeLink -> ExchangeLink.of(exchangeLink.attr("href")))
+                .collect(Collectors.toList());
     }
 
-    public List<ExchangeRate>
+    public List<ExchangeRate> parseExchangeRates(String payload) {
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xPath = xPathFactory.newXPath();
+
+        InputSource source = new InputSource(new StringReader(payload));
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = null;
+        org.w3c.dom.Document doc;
+        try {
+            db = dbf.newDocumentBuilder();
+            doc = db.parse(source);
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            log.debug("Unexpected exception occurred while parsing exchange rates xml");
+            return Collections.emptyList();
+        }
+
+        doc.getElementsByTagName()
+    }
 }
--- src/main/java/com/currency/scraper/scraping/Scraper.java
+++ src/main/java/com/currency/scraper/scraping/Scraper.java
@@ -1,15 +1,60 @@
 package com.currency.scraper.scraping;
 
+import com.currency.scraper.configuration.ScraperProperties;
+import com.currency.scraper.entity.ExchangeRate;
+import com.currency.scraper.vo.ExchangeLink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class Scraper {
 
-    private URI baseAddress;
+    private Client client;
 
-    private URI rssAddress;
+    private Parser parser;
 
+    private ScraperProperties scraperProperties;
 
+    private static final Logger log = LoggerFactory.getLogger(Scraper.class);
+
+    public Scraper(Client client, ScraperProperties scraperProperties, Parser parser) {
+        this.client = client;
+        this.scraperProperties = scraperProperties;
+        this.parser = parser;
+    }
+
+    private URI createAddress(URI endpoint) {
+        return UriComponentsBuilder.fromUri(scraperProperties.getBaseAddress())
+                .pathSegment(endpoint.toString())
+                .build()
+                .encode()
+                .toUri();
+    }
+
+    private URI getExchangeRssAddress() {
+        return createAddress(scraperProperties.getRssAddress());
+    }
+
+    private List<ExchangeLink> fetchExchangeLinks() {
+        URI address = getExchangeRssAddress();
+        return parser.parseExchangeLinks(client.fetchResource(address));
+    }
+
+    private List<ExchangeRate> fetchExchangeRates(List<ExchangeLink> links) {
+        return links.stream()
+                .map(link -> parser.parseExchangeRates(client.fetchResource(createAddress(link.getLink()))))
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+    }
+
+    public List<ExchangeRate> scrape() {
+        List<ExchangeLink> links = fetchExchangeLinks();
+        return fetchExchangeRates(links);
+    }
 }
